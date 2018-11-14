ed9ac0c3ca20d753187b2afa1dd4e1deb3d30d79
unknown <akinyeleolat2005@gmail.com>
2018-11-14 03:17:35 +0100

    fix redirect

--- UI/src/js/fetch/register.js
+++ UI/src/js/fetch/register.js
@@ -25,7 +25,8 @@ const createAccount = (url,dataBody,redirectHome) => {
     .then((res) => res.json())
     .then((data) =>{
       if(data.success){
-        window.location.replace(`${redirectHome}`)
+        // window.location.replace(`${redirectHome}`)
+        document.getElementById('responseMessage').innerHTML = data.message+` Kindly await the admin to activate your account`
         console.log(data)
        }
        else{

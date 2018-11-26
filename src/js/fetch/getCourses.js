ee3b4c95a523f148ca5bda0a71b8f9a229ced803
unknown <akinyeleolat2005@gmail.com>
2018-11-26 10:56:32 +0100

    ft-add-candidatepage

--- src/js/fetch/getCourses.js
+++ src/js/fetch/getCourses.js
@@ -1,61 +1,5 @@
 const token = localStorage.getItem('token')
 
-const addCourses = (event) => {
-  event.preventDefault()
-  //  get form input
-  const courseTitle = document.getElementById('courseTitle').value.trim()
-  const courseDescription = document.getElementById('course_description').value.trim()
-  if (!token) {
-    window.location.replace('./admin_login.html')
-    // eslint-disable-next-line no-alert
-    alert('Kindly login or create Account')
-  }
-  else {
-    // document.getElementById('responseMessage').innerHTML = courseTitle
-    const dataBody = JSON.stringify({
-      courseTitle,
-      courseDescription
-    })
-    const URL = 'https://cbtng.herokuapp.com/api/v1/courses'
-    createCourses(URL, dataBody)
-  }
-}
-const createCourses = (URL, dataBody) => {
-  const bearer = `${token}`
-  fetch(URL, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'token':bearer,
-      "Access-Control-Allow-Origin": "*",
-      },
-    body:dataBody
-    })
-    .then((res) => {
-      if (res.status === '401') {
-        // eslint-disable-next-line no-alert
-        alert('Access denied')
-        window.location.replace('./adminHome.html')
-      }
-      else {
-        return res.json()
-      }
-    })
-    .then((data) => {
-      if (data.success === 'true') {
-        document.getElementById('responseMessage').innerHTML = `${data.message}`
-      }
-      else {
-        document.getElementById('responseMessage').innerHTML = `${data.message}`
-      }
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.log(error)
-    })
-}
-
 const fetchCourses = () => {
   const URL = 'https://cbtng.herokuapp.com/api/v1/courses/approve'
   const bearer = `${token}`
@@ -117,25 +61,7 @@ const fetchCourses = () => {
       console.log(error)
     })
 }
-const approveCourses = () => {
-  console.log('welcome')
-}
-const getApproval = () => {
-  const elementsArray = document.querySelectorAll('.btn btn-block btn-primary approveBtn')
-  console.log(elementsArray)
 
-  elementsArray.forEach((elem) => {
-    elem.addEventListener('click', () => {
-    // this function does stuff
-      if (event.target.parentElement.classList.contains('btn btn-block btn-primary approveBtn')) {
-        console.log('button clicked')
-        approveCourses()
-      }
-    })
-  })
-}
-
-document.getElementById('addCourses').addEventListener('submit', addCourses)
 window.onload = function () {
   if (!token) {
     window.location.replace('./admin_login.html')
@@ -143,10 +69,6 @@ window.onload = function () {
     alert('Kindly login or create Account')
   } else {
     fetchCourses()
-    getApproval()
-    // if (fetchCourses) {
-    //   getApproval()
-    // }
   }
 }
 
